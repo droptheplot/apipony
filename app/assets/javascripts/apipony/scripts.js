6fbac1985f5f73e7302c6b2d3fa90ebcc83e1284
Sergey Novikov <novikov359@gmail.com>
2015-11-27 03:20:47 +0300

    Update styles, add sidebar

--- app/assets/javascripts/apipony/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.endpoint h3').on('click', function() {
-    if($(this).next('.more').length > 0) {
-      $(this).next('.more').slideToggle();
-      $(this).prev('.line').toggle();
-    }
-  });
-});
