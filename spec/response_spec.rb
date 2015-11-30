62c7ab6e65f5ff37b5a9849249761a65047d7e88
Anthony Super <anthony@noided.media>
2015-11-30 13:44:17 -0700

    .swp

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -9,5 +9,19 @@ RSpec.describe Apipony::Response do
       expect(r.example.body).to be_a(Array)
       expect(r.example.body.first).to eq({name: "Test"})
     end
+    it "serializes from attributes" do
+      r = Apipony::Response.new 200 do 
+        attribute :name, example: "Test"
+        attribute :foo do
+          attribute :id, example: 1
+        end
+      end
+      expect(r.example.body).to eq({
+        name: "Test",
+        foo: {
+          id: 1
+        }
+      })
+    end
   end
 end
