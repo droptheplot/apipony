c9bc9c84c5f92988a3c535dad1dbcebbc585b12e
Sergey Novikov <novikov359@gmail.com>
2016-06-12 23:31:26 +0300

    Remove example_response.rb

--- lib/apipony/example_response.rb
+++
@@ -1,9 +0,0 @@
-##
-# Display an example response to help with understanding of the API.
-class Apipony::ExampleResponse < Apipony::Base
-  attr_accessor :headers, :body
-  def initialize(&block)
-    instance_eval(&block) if block_given?
-  end
-
-end
